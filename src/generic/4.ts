33290d310aa6168cf967df4a77333f06cc361e93
oleksandrkravcuk <134079899+oleksandrkravcuk@users.noreply.github.com>
2024-01-30 15:53:49 +0200

    work2

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface ComponentProps {
+  title: string;
+}
+
+class Component <T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<{title: ComponentProps}> {
   pageInfo () {
     console.log(this.props.title);
   }
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -8,14 +8,14 @@
 */
 
 type User = {
-  name: string;
-  surname: string;
-  email: string;
-  password: string;
+  name?: string;
+  surname?: string;
+  email?: string;
+  password?: string;
 }
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
+function createOrUpdateUser(initialValues: Partial<User>) {
+
 }
 
 createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
